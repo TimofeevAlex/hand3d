39efc8df6a0920a804fe92c72ac2037b4b7e4bae
Alexander Timofeev <timofeev.am@gmail.com>
2019-08-03 04:25:37 +0300

    ADD: the new instance for the test task

--- run.py
+++ run.py
@@ -1,22 +1,6 @@
-#
-#  ColorHandPose3DNetwork - Network for estimating 3D Hand Pose from a single RGB Image
-#  Copyright (C) 2017  Christian Zimmermann
-#  
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 from __future__ import print_function, unicode_literals
 
+import os
 import tensorflow as tf
 import numpy as np
 import scipy.misc
@@ -24,19 +8,43 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 from nets.ColorHandPose3DNetwork import ColorHandPose3DNetwork
-from utils.general import detect_keypoints, trafo_coords, plot_hand, plot_hand_3d
+from utils.general import detect_keypoints, trafo_coords, plot_hand_3d
 
-if __name__ == '__main__':
-    # images to be shown
-    image_list = list()
-    image_list.append('./data/img.png')
-    image_list.append('./data/img2.png')
-    image_list.append('./data/img3.png')
-    image_list.append('./data/img4.png')
-    image_list.append('./data/img5.png')
+HEIGHT, WIDTH = 240, 320
 
-    # network input
-    image_tf = tf.placeholder(tf.float32, shape=(1, 240, 320, 3))
+def plot_hand(coords_hw, axis, color_fixed=None,  markersize=1, alpha=1.0, fillstyle='full'):
+    """ Plots a hand stick figure into a matplotlib figure. """
+    # define connections and colors of the bones
+    bones = [
+             (0, 4), (4, 3), (3, 2), (2, 1), 
+             (0, 8), (8, 7), (7, 6), (6, 5), 
+             (0, 12), (12, 11), (11, 10), (10, 9), 
+             (0, 16), (16, 15), (15, 14), (14, 13),
+             (0, 20), (20, 19), (19, 18), (18, 17)
+            ]
+
+    for connection in bones:
+        coord1 = coords_hw[connection[0], :]
+        coord2 = coords_hw[connection[1], :]
+        coords = np.stack([coord1, coord2])
+        axis.plot(coords[:, 1], coords[:, 0], color_fixed,  markersize=markersize, \
+                  alpha=alpha, fillstyle=fillstyle)
+
+def _main():
+    
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input_image', required=True,
+                        help='Input image file.')
+
+    args = parser.parse_args()
+    img_name = args.input_image
+
+    if not os.path.exists(img_name):
+        raise Exception('an image `{}` does not exist'.format(args.input_image))
+
+     # network input
+    image_tf = tf.placeholder(tf.float32, shape=(1, HEIGHT, WIDTH, 3))
     hand_side_tf = tf.constant([[1.0, 0.0]])  # left hand (true for all samples provided)
     evaluation = tf.placeholder_with_default(True, shape=())
 
@@ -52,41 +60,53 @@ if __name__ == '__main__':
     # initialize network
     net.init(sess)
 
-    # Feed image list through network
-    for img_name in image_list:
-        image_raw = scipy.misc.imread(img_name)
-        image_raw = scipy.misc.imresize(image_raw, (240, 320))
-        image_v = np.expand_dims((image_raw.astype('float') / 255.0) - 0.5, 0)
+    image_raw = scipy.misc.imread(img_name)
+    image_raw = scipy.misc.imresize(image_raw, (HEIGHT, WIDTH))
+    image_v = np.expand_dims((image_raw.astype('float') / 255.0) - 0.5, 0)
 
-        hand_scoremap_v, image_crop_v, scale_v, center_v,\
-        keypoints_scoremap_v, keypoint_coord3d_v = sess.run([hand_scoremap_tf, image_crop_tf, scale_tf, center_tf,
-                                                             keypoints_scoremap_tf, keypoint_coord3d_tf],
-                                                            feed_dict={image_tf: image_v})
+    hand_scoremap_v, image_crop_v, scale_v, center_v, \
+    keypoints_scoremap_v, keypoint_coord3d_v = sess.run(
+                                  [
+                                    hand_scoremap_tf, image_crop_tf, 
+                                    scale_tf, center_tf,
+                                    keypoints_scoremap_tf,
+                                    keypoint_coord3d_tf
+                                  ],
+                                  feed_dict={image_tf: image_v}
+                                 )
 
-        hand_scoremap_v = np.squeeze(hand_scoremap_v)
-        image_crop_v = np.squeeze(image_crop_v)
-        keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
-        keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
+    hand_scoremap_v = np.squeeze(hand_scoremap_v)
+    keypoints_scoremap_v = np.squeeze(keypoints_scoremap_v)
+    keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
+    
+    # post processing
+    coord_hw_crop = detect_keypoints(np.squeeze(keypoints_scoremap_v))
+    coord_hw = trafo_coords(coord_hw_crop, center_v, scale_v, 256)
+    
+    #output coordinates
+    print('Key points:')  #do inverse transformation of coordinates
+    print('|{:<4s}|{:<8s}|{:<8s}|{:<8s}|'.format('№', 'x', 'y', 'z'))
+    for idx, point in enumerate(keypoint_coord3d_v):
+        print('|{:<4}|{:< 8.4f}|{:< 8.4f}|{:< 8.4f}|'.format(idx, *point))
+    
+    # visualize
+    fig = plt.figure(1)
+    
+    ax1 = fig.add_subplot(221)
+    ax1.imshow(image_raw) 
+    plot_hand(coord_hw, ax1, color_fixed='ro', markersize=5)
+    ax1.set_title('Key Points')
+    
+    ax2 = fig.add_subplot(222)
+    max_points_scoremap = np.argmax(hand_scoremap_v, 2)
+    hand_scoremap_v = max_points_scoremap - np.min(max_points_scoremap)
+    hand_area = hand_scoremap_v > 0.0
+    
+    image_raw[hand_area, 1] = 255.0
+    ax2.imshow(image_raw)   
+    ax2.set_title('Painted Hand')
 
-        # post processing
-        image_crop_v = ((image_crop_v + 0.5) * 255).astype('uint8')
-        coord_hw_crop = detect_keypoints(np.squeeze(keypoints_scoremap_v))
-        coord_hw = trafo_coords(coord_hw_crop, center_v, scale_v, 256)
-
-        # visualize
-        fig = plt.figure(1)
-        ax1 = fig.add_subplot(221)
-        ax2 = fig.add_subplot(222)
-        ax3 = fig.add_subplot(223)
-        ax4 = fig.add_subplot(224, projection='3d')
-        ax1.imshow(image_raw)
-        plot_hand(coord_hw, ax1)
-        ax2.imshow(image_crop_v)
-        plot_hand(coord_hw_crop, ax2)
-        ax3.imshow(np.argmax(hand_scoremap_v, 2))
-        plot_hand_3d(keypoint_coord3d_v, ax4)
-        ax4.view_init(azim=-90.0, elev=-90.0)  # aligns the 3d coord with the camera view
-        ax4.set_xlim([-3, 3])
-        ax4.set_ylim([-3, 1])
-        ax4.set_zlim([-3, 3])
-        plt.show()
+    plt.show()
+    
+if __name__ == '__main__':
+    _main()
